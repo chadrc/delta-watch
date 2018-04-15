33df655eb6163883b29cccb5e23b0a183f6f824b
chadcollins <chadrcollins91@gmail.com>
2018-04-15 08:00:38 -0400

    (delta-watch) clean script now suppresses errors (delta-watch) index now default exports necessary functions instead of entire DeltaWatch class

--- packages/delta-watch/src/index.ts
+++ packages/delta-watch/src/index.ts
@@ -1,1 +1,13 @@
-export {DeltaWatch} from './core/DeltaWatch';
+import {DeltaWatch, Watchable} from './core/DeltaWatch';
+
+export default {
+  Make: (data: object) => {
+    return new DeltaWatch(data);
+  },
+  Watch: (watchable: Watchable, cb: Function) => {
+    return DeltaWatch.watch(watchable, cb);
+  },
+  Unwatch: (watchable: Watchable, cb: Function) => {
+    return DeltaWatch.unwatch(watchable, cb);
+  }
+}
