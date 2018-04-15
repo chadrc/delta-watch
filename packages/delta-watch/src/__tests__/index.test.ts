98a74ce91eee6f5badf5609a79b3e4ef85a95d7c
chadcollins <chadrcollins91@gmail.com>
2018-04-15 08:25:02 -0400

    (delta-watch) Added unwatching to index test

--- packages/delta-watch/src/__tests__/index.test.ts
+++ packages/delta-watch/src/__tests__/index.test.ts
@@ -1,23 +1,33 @@
 import {expect} from 'chai';
 import 'jest';
-import {assertWatcherCalled} from "./utils";
+import {assertWatcherCalled, assertWatcherNotCalled} from "./utils";
 import DeltaWatch from '../index';
 
 describe(`Testing package exports`, () => {
-  it(`Work just like their internal counterparts`, () => {
+  it(`Can watch, mutate, and access just like internal counterparts`, () => {
     let watchable = DeltaWatch.Watchable({
       value: "Starting Value"
     });
 
     let watcherCalled = false;
-    DeltaWatch.Watch(watchable.Watcher.value, (value: string) => {
+    const cb = (value: string) => {
       expect(value).to.equal("New Value");
       watcherCalled = true;
-    });
+    };
+
+    DeltaWatch.Watch(watchable.Watcher.value, cb);
 
     watchable.Mutator.value = "New Value";
 
     expect(watchable.Accessor.value).to.equal("New Value");
     assertWatcherCalled(watcherCalled);
+
+    DeltaWatch.Unwatch(watchable.Watcher.value, cb);
+
+    watcherCalled = false;
+
+    watchable.Mutator.value = "Next Value";
+
+    assertWatcherNotCalled(watcherCalled);
   });
 });
