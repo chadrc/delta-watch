ce32aa7a631c4de255f190daed7a85d5d73e737c
chadcollins <chadrcollins91@gmail.com>
2018-04-20 22:32:18 -0400

    (examples) Imported setup modules

--- packages/examples/pages/notes/notes.ts
+++ packages/examples/pages/notes/notes.ts
@@ -1,10 +1,21 @@
 import DeltaWatch from 'delta-watch';
+import setupMutations from './mutations';
+import setupWatches from './watches';
 declare const M: any; // Materialize CSS global
 
 window.addEventListener('load', () => {
   const elements = {
-    todoList: document.getElementById('todoList')
+    todoList: document.getElementById('todoList'),
+    selectedNoteName: document.getElementById('selectedNoteName'),
+    selectedNoteText: document.getElementById('selectedNoteText')
   };
 
   M.Collapsible.init(elements.todoList, {});
+
+  const {Watcher, Mutator, Accessor} = DeltaWatch.Watchable({
+
+  });
+
+  setupWatches(elements, Accessor, Watcher);
+  setupMutations(elements, Accessor, Mutator);
 });
