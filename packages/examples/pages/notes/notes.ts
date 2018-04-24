b794fabf1f9da53bd2ac19354804906b5a095cfd
chadcollins <chadrcollins91@gmail.com>
2018-04-23 22:10:02 -0400

    (examples) Began implementing note creation

--- packages/examples/pages/notes/notes.ts
+++ packages/examples/pages/notes/notes.ts
@@ -1,7 +1,7 @@
 import DeltaWatch from 'delta-watch';
 import setupMutations from './mutations';
 import setupWatches from './watches';
-import {NoteCollection} from "./utils";
+import {Note, NoteCollection} from "./utils";
 declare const M: any; // Materialize CSS global
 
 window.addEventListener('load', () => {
@@ -23,8 +23,26 @@ window.addEventListener('load', () => {
     noteCollections: [],
     selectedTodoList: null,
     selectedTodo: null,
-    collectionName: ''
+    collectionName: '',
+    newNoteInputsByIndex: {}
   });
+
+  function createNote(collectionIndex: number, name: string) {
+    let newNote: Note = {
+      name: name,
+      text: ''
+    };
+
+    console.log('adding note', newNote);
+
+    let clone = document.importNode(elements.noteTemplate.content, true);
+    let link = clone.querySelector('a');
+    let index = Accessor.noteCollections[collectionIndex].notes.length;
+
+    DeltaWatch.Watch(Watcher.noteCollections[collectionIndex].notes[index].name, (name: string) => {
+      link.innerHTML = name;
+    });
+  }
 
   const methods = {
     createCollectionItem: (name: string) => {
@@ -37,6 +55,7 @@ window.addEventListener('load', () => {
       let header = clone.querySelector('.collapsible-header > .note-text');
       let deleteBtn = clone.querySelector('.collapsible-header > a');
       let list = clone.querySelector('.collapsible-body ul');
+      let newNoteInput = clone.querySelector('.collapsible-body ul li:last-of-type input') as HTMLInputElement;
       let index = Accessor.noteCollections.length;
 
       DeltaWatch.Watch(Watcher.noteCollections[index].name, (name: string) => {
@@ -49,6 +68,20 @@ window.addEventListener('load', () => {
 
         Mutator.noteCollections.splice(index, 1);
         elements.noteList.removeChild(elements.noteList.children[elements.noteList.children.length - 1]);
+      });
+
+      newNoteInput.addEventListener('input', (event) => {
+        let t = event.target as HTMLInputElement;
+        Mutator.newNoteInputsByIndex[index] = t.value;
+      });
+
+      newNoteInput.addEventListener('keyup', (event: KeyboardEvent) => {
+        if (event.code === "Enter") {
+          let newNoteName = Accessor.newNoteInputsByIndex[index];
+          createNote(index, newNoteName);
+          newNoteInput.value = '';
+          Mutator.newNoteInputsByIndex[index] = '';
+        }
       });
 
       elements.noteList.appendChild(clone);
