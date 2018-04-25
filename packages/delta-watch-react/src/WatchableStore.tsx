3dad367a6c3c1526f4eb5af06bb6f346365da822
chadcollins <chadrcollins91@gmail.com>
2018-04-24 21:10:39 -0400

    (delta-watch-react) Mutation re-render test

--- packages/delta-watch-react/src/WatchableStore.tsx
+++ packages/delta-watch-react/src/WatchableStore.tsx
@@ -1,12 +1,50 @@
 import * as React from "react";
+import DeltaWatch from 'delta-watch';
 
-function MakeStore(data: any) {
-  let Watch = () => {
+export interface DeltaWatchStore {
+  Watch: Function,
+  Store: {
+    Mutator: any,
+    Accessor: any,
+    Watcher: any
+  }
+}
+
+function MakeStore(data: any): DeltaWatchStore {
+  let watchable = DeltaWatch.Watchable(data);
+  let Watch = (
+    mapWatchers: (watcher: any, props: any) => { [key: string]: any }
+  ) => {
     return (Target: any) => {
-      let c: any = class extends React.Component {
+      let c: any = class extends React.Component<any> {
+        private watchers: { [key: string]: any };
+        constructor(props: any) {
+          super(props);
+          this.makeWatchers(props);
+        }
+
+        makeWatchers(props: any) {
+          this.watchers = mapWatchers(watchable.Watcher, props);
+          for (let field of Object.keys(this.watchers)) {
+            DeltaWatch.Watch(this.watchers[field], this.update);
+          }
+        }
+
+        componentWillReceiveProps(nextProps: any, nextContext: any) {
+          this.makeWatchers(nextProps);
+        }
+
+        update = () => {
+          this.forceUpdate();
+        };
+
         render() {
+          let watchProps: { [key: string]: any } = {};
+          for (let field of Object.keys(this.watchers)) {
+            watchProps[field] = this.watchers[field]._data;
+          }
           return (
-            <Target {...this.props} />
+            <Target {...this.props} {...watchProps} />
           )
         }
       };
@@ -18,7 +56,8 @@ function MakeStore(data: any) {
   };
 
   return {
-    Watch
+    Watch,
+    Store: watchable
   }
 }
 
--- packages/delta-watch-react/src/__tests__/index.test.tsx
+++ packages/delta-watch-react/src/__tests__/index.test.tsx
@@ -1,12 +1,12 @@
 import 'jest';
 import * as React from 'react';
-import {expect} from 'chai';
+import {expect as chaiExpect} from 'chai';
 import DeltaWatchReact from '../index';
 import * as renderer from 'react-test-renderer';
 
 describe(`Core`, () => {
   it(`Module loads`, () => {
-    expect(DeltaWatchReact).to.exist;
+    chaiExpect(DeltaWatchReact).to.exist;
   });
 
   it(`MakeStore and Watch create a renderable component`, () => {
@@ -14,5 +14,37 @@ describe(`Core`, () => {
     let comp = () => <div/>;
     let Wrapped = watchableStore.Watch()(comp);
     renderer.create(<Wrapped/>);
+  });
+
+  it(`Mutating a watchable store causes a wrapped component to re-render`, () => {
+    let {Watch, Store} = DeltaWatchReact.MakeStore({
+      message: "Hello"
+    });
+
+    let renderCount = 0;
+    class Text extends React.Component<any, any> {
+      render() {
+        renderCount++;
+        return (
+          <p>{this.props.message}</p>
+        );
+      }
+    }
+
+    let Wrapped = Watch((watcher: any) => ({
+      message: watcher.message
+    }))(Text);
+
+    let component = renderer.create(<Wrapped/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    Store.Mutator.message = "Delta Watch Render";
+
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // 1 for initial render, 1 for re-render
+    chaiExpect(renderCount).to.equal(2);
   });
 });
