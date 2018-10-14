385797cd510c31d6c7df59b671012fc3caf97c63
chadcollins <chadrcollins91@gmail.com>
2018-10-14 16:33:14 -0400

    Added to test class

--- packages/delta-watch/src/__tests__/TypeRegistry.test.ts
+++ packages/delta-watch/src/__tests__/TypeRegistry.test.ts
@@ -2,10 +2,25 @@ import DeltaWatch from '../';
 import {expect} from 'chai';
 import 'jest';
 
+let noteCount = 0;
+
 class Note {
-    id: string;
-    text: string;
-    priority: number;
+    _id: number;
+    _text: string;
+    _priority: number;
+    _completed: boolean;
+
+    public Note(text: string, priority: number) {
+        this._id = noteCount++;
+        this._text = text;
+        this._priority = priority;
+        this._completed = false;
+    }
+
+    finish() {
+        this._completed = true;
+        this._priority = null;
+    }
 }
 
 describe(`Type Registration`, () => {
